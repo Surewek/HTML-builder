1550c6b181de824dc71c40213e699ac2e338eb30
Vitaly Vegera <surewek@gmail.com>
2022-11-08 21:32:24 +0300

    fix: replace console.log with stdout

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
+const { stdout } = require('process');
 
 const filePath = path.join(__dirname, 'text.txt');
+const readble = fs.createReadStream(filePath, 'utf-8');
 
-fs.readFile(filePath, 'utf-8', (err, content) => console.log(err ?? content));
+readble.pipe(stdout);
